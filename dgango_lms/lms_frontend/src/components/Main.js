4fa2098f61676604ea2da885023c85dab9791138
Sruthithandoju <67855046+Sruthithandoju@users.noreply.github.com>
2022-05-19 19:26:57 +0530

    Add files via upload

---
+++ dgango_lms/lms_frontend/src/components/Main.js
@@ -0,0 +1,13 @@
+import Home from './Home'
+import Header from './Header'
+
+function Main() {
+  return (
+    <div className="App">
+    <Header/>
+     <Home/>
+    </div>
+  );
+}
+
+export default Main;
